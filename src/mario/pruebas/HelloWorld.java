7fd041777ca01ad824df39fc7998eeb0170a03fb
Mario <Mario@Mario-PC>
2020-11-04 13:38:29 +0100

    13.38

---
+++ src/mario/pruebas/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mario.pruebas;
+
+/**
+ *
+ * @author Mario
+ */
+public class HelloWorld {
+      public static void main(String[] args) {		
+		System.out.println("Hola Mundo3333333");
+	}
+}
